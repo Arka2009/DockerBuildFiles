42a47483963498ccda6e9734da9899696952fd43
Arka <amaity@wasteviz.tech>
2021-11-26 09:12:38 +0800

    Bugs found while COPYing gitconfig from the Dockerfile

--- imageBuild_LLVMLatest.sh
+++ imageBuild_LLVMLatest.sh
@@ -5,8 +5,11 @@ dockerImage="arka2009/llvm-13.0.0:v0"
 dockerFile="${BUILDROOT}/DockerFiles/LLVMLatest.Dockerfile"
 dockerBuildContext="${BUILDROOT}/DockerBuildContexts"
 sshPrivKeyFile="SSHKeys/id_rsa_docker" # This path is relative to ${dockerBuildContext}
+vimrcFile="Configs/vimrc"
+gitConfigFile="Configs/gitconfig"
 
 docker build -t ${dockerImage} \
         --build-arg SSH_PRIVKEY_FILE=${sshPrivKeyFile} \
+        --build-arg VIMRCFILE=${vimrcFile} \
         -f ${dockerFile} \
         ${dockerBuildContext}
